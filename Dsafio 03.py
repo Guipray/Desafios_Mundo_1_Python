61e9319df7fac558a66dd19a390c3f76e6caf0e6
Guilherme Henrique <109701399+Guipray@users.noreply.github.com>
2022-11-19 12:02:40 -0300

    Desafio 3 adicionado!

---
+++ Dsafio 03.py
@@ -0,0 +1,5 @@
+print('------DESAFIO 3------')
+num1 = int(input('Digite um número:'))
+num2 = int(input('Digite um segundo número:'))
+s = num1 + num2
+print(f'A soma entre os números {num1} e {num2} é {s}')
